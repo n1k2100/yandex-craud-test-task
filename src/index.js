9201311632f10d80e4cf1ee5ed85650c31733c50
n1k <n1k@the-dev.ru>
2026-05-07 03:10:21 +0500

    #34 chore: изменена структура проекта под vite

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles/fonts.css";
+import "./styles/stages.css";
+import "./styles/participants.css";
+import "./styles/marquee.css";
+import "./styles/common.css";
+import "./index.css";
+
+import "./scripts/marquee";
+import "./scripts/stages";
+import "./scripts/participants";
