fe1655f75387803c40b44afe24ffa3bb094a7c83
n1k <n1k@the-dev.ru>
2026-05-07 03:45:22 +0500

    #36 style: стандартизировано написание стилей
    > Сначала общие стили, затем мобильные, потом десктоп.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import "./styles/stages.css";
 import "./styles/participants.css";
 import "./styles/marquee.css";
 import "./styles/common.css";
-import "./index.css";
+import "./styles/base.css";
+import "./styles/decor.css";
+import "./styles/header.css";
+import "./styles/info.css";
+import "./styles/hero.css";
+import "./styles/footer.css";
 
 import "./scripts/marquee";
 import "./scripts/stages";
